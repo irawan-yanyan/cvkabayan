9c10f67ec513b3ef861b61cd0c3888f0be0535ce
irawan-yanyan <irawan.yanyan@yahoo.com>
2020-01-27 16:04:26 +0700

    first commit code factorial

---
+++ faktorial.php
@@ -0,0 +1,42 @@
+<?php
+ class faktorial{
+  function countFaktorial($bilangan){
+	   if($bilangan>=1){
+  			echo "Angka Faktorial :\n";
+  				for($i=1; $i <= $bilangan; $i++){
+					     $faktorial = 1; 
+					     echo $i. "! = ";
+       						     for($j=$i; $j > 0; $j--) {     
+    							if($j == 1){
+							            echo " 1 = " .$faktorial;
+        						}else{  
+						      			echo $j ." x ";     
+						      	}       
+						         	$faktorial*=$j; 
+     						     }echo "\n";
+  				}
+	  }else if($bilangan == 0){
+			   echo "0! = 1";
+	  }else{
+		  // validasi jika yang angka yang dimasukan < nol
+		  echo "Maaf, Anda memasukkan bilangan < 0 .";
+	}
+	     echo "angka faktorial dari : ".$bilangan." adalah : ".$faktorial ;
+	     
+   }
+}
+
+	$nilai = readline("angka yang di masukan : ");
+	if(is_numeric($nilai)){
+		// set object faktorial
+		$objFaktorial = new faktorial();
+		$objFaktorial->countFaktorial($nilai);
+	}else{
+		echo "Maaf, input yang anda masukan bukan angka.";
+	}
+
+
+
+
+
+?>
